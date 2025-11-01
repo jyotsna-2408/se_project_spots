81ec010543d8c1abe9b9dbfe6276d2d73c89cb22
JYOTSNA SHRIVASTAVA <JYOTSNA.2408@GMAIL.COM>
2025-11-01 14:00:40 -0400

    updates - 11/1

--- scripts/index.js
+++ scripts/index.js
@@ -18,41 +18,55 @@ const editProfileNameInput = editProfileModal.querySelector(
 const editProfileDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input"
 );
-const newPostImageLink = newPostModal.querySelector("#card-image-link");
+const newPostImageUrlInput = newPostModal.querySelector("#card-image-link");
 const newPostCaptionInput = newPostModal.querySelector("#caption-input");
+
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
 
 editProfileButton.addEventListener("click", function () {
   editProfileNameInput.value = profileNameElement.textContent;
   editProfileDescriptionInput.value = profileDescriptionElement.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
+  //editProfileModal.classList.add("modal_is-opened");
 });
 
 function handleEditFormSubmission(evt) {
   evt.preventDefault();
   profileNameElement.textContent = editProfileNameInput.value;
   profileDescriptionElement.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
+  //editProfileModal.classList.remove("modal_is-opened");
 }
 
 editProfileForm.addEventListener("submit", handleEditFormSubmission);
 
 editProfileCloseButton.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
+  // editProfileModal.classList.remove("modal_is-opened");
 });
 
 addNewPostButton.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
+  //newPostModal.classList.add("modal_is-opened");
 });
 
 function handleNewPostFormSubmission(evt) {
   evt.preventDefault();
-  console.log("Image input element:", newPostImageLink.value);
+  console.log("Image input element:", newPostImageUrlInput.value);
   console.log("Caption input element:", newPostCaptionInput.value);
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
+  //newPostModal.classList.remove("modal_is-opened");
 }
 
 newPostForm.addEventListener("submit", handleNewPostFormSubmission);
 
 newPostCloseButton.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
+  //newPostModal.classList.remove("modal_is-opened");
 });
